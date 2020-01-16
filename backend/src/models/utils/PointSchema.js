f21c8997ec340bba7ab4978f7a133a100b418b47
Alexandre Ferreira <alexmlf93@gmail.com>
2020-01-16 02:47:27 -0300

    Abstração para a controller.

--- backend/src/models/utils/PointSchema.js
+++ backend/src/models/utils/PointSchema.js
@@ -4,12 +4,12 @@ const PointSchema = new mongoose.Schema({
     type: {
         type: String,
         enum: ['Point'],
-        required: true,
+        required: true
     },
     coordinates: {
         type: [Number],
-        required: true,
-    },
+        required: true
+    }
 });
 
 module.exports = PointSchema;
